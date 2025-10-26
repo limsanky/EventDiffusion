c52323ef44b9bf312069c3f2f21391dc9772404e
limsanky <sankarshana.v@gmail.com>
2025-10-26 16:49:21 +0000

    mvsec dataset fixed

--- mvsec_dataset.py
+++ mvsec_dataset.py
@@ -28,36 +28,43 @@ class SingleMVSECSampler(Sampler):
             # print(f'Number of samples for {self.scenario}{exp} in split {self.split}: {count[exp]}')
 
         self.count = count
-
-    # def __iter__(self):
-    #     indices = []
-    #     for exp in self.training_splits:
-    #         idxs = list(range(1, self.count[exp] + 1))
-    #         random.shuffle(idxs)
-    #         indices.append(idxs)
-
-    #     for first, second in zip(*indices):
-    #         yield [ first, second ]
     
     def __iter__(self):
-        indices = []
+        # indices = []
         
-        for exp in self.training_splits:
-            # idxs = list(range(self.indices[exp][0] + 1, self.indices[exp][1] + 1))
-            # assert len(idxs) == self.count[exp], f"Length mismatch for {self.scenario}{exp} in split {self.split}: expected {self.count[exp]}, got {len(idxs)}"
-            # for _i in range(len(idxs)):
-            #     assert idxs[_i] > self.indices[exp][0], f"Index out of bounds: {idxs[_i]} is smaller than or equal to {self.indices[exp][0]}"
-            #     assert idxs[_i] <= self.indices[exp][1], f"Index out of bounds: {idxs[_i]} is greater than {self.indices[exp][1]}"
-            idxs = list(range(1, self.count[exp] + 1))
-            random.shuffle(idxs)
-            indices.append(idxs)
+        # for exp in self.training_splits:
+        #     idxs = list(range(1, self.count[exp] + 1))
+        #     # random.shuffle(idxs)
+        #     indices.append(idxs)
 
-        for first, second in zip(*indices):
-            yield [ first, second ]
+        # for first, second in zip(*indices):
+        #     yield [ first, second ]
+        
+        return_indices = []
+        all_indices = np.random.randint(0, self.__len__(), size=(self.__len__(),))
+        # all_indices = np.random.randint(1415, 1425, size=(10,))
+        total_count = 0
+
+        for exp in self.training_splits:
+            split_count = self.count[exp]
+            for i in all_indices:
+                if (i < total_count + split_count) and (i >= total_count):
+                    idx = i - total_count
+                    # if total_count != 0:
+                        # print(idx, i)
+                        # exit()
+                    # return_indices.append((i, exp))
+                    return_indices.append((idx, exp))
+            total_count += split_count
+        # exit()
+        assert len(return_indices) == self.__len__(), f'Length mismatch: {len(return_indices)} vs {self.__len__()}'
+
+        for idx, exp in return_indices:
+            yield (idx, exp)
+        
     
     def __len__(self):
         output = sum(self.count.values())
-        output = 2 * output # Since there are left AND right cameras
         return output
 
 class MVSECSampler(BatchSampler):
@@ -125,13 +132,13 @@ class MVSECDataset(Dataset):
         for loc in self.loc:
             data = {}
             for exp in self.training_splits:
-                ev_rep_path = os.path.join(self.data_dir, f'{self.scenario}/{self.scenario}{exp}/evrep_train/evrep_{loc}.npy')
-                # ev_rep_path = os.path.join(self.data_dir, f'{self.scenario}/{self.scenario}{exp}/evrep_train/evrep_{loc}.pt')
+                # ev_rep_path = os.path.join(self.data_dir, f'{self.scenario}/{self.scenario}{exp}/evrep_train/evrep_{loc}.npy')
+                ev_rep_path = os.path.join(self.data_dir, f'{self.scenario}/{self.scenario}{exp}/evrep_train/evrep_{loc}.pt')
                 assert os.path.exists(ev_rep_path), f'EvRep file does not exist: {ev_rep_path}'
                 # print(f'Loading EvRep for {self.scenario}{exp} at {loc} camera.')
-                ev_rep = np.load(ev_rep_path)
-                data[exp] = torch.from_numpy(ev_rep)
-                # data[exp] = torch.load(ev_rep_path)
+                # ev_rep = np.load(ev_rep_path)
+                # data[exp] = torch.from_numpy(ev_rep)
+                data[exp] = torch.load(ev_rep_path)
                 
             event_data[loc] = data
             
@@ -165,25 +172,67 @@ class MVSECDataset(Dataset):
         image_data = {}
         event_data = {}
         
+        idx, split = index
+        
         for loc in self.loc:
             
             loc_event_data: dict = self.event_data[loc]
             
-            event_data_at_loc = [ (loc_event_data.get(s)[i], i) for s, i in zip(self.training_splits, index) ]
+            event_data_at_loc =  loc_event_data.get(split)
+            # print('idx, split:', idx, split)
+            # print('event_data_at_loc', event_data_at_loc.shape)
+            event_data_at_loc = event_data_at_loc[idx]
+            # print('event_data_at_loc[idx]', event_data_at_loc.shape)
+            # exit()
             
             if self.event_transforms:
-                event_data_at_loc = [ (self.event_transforms(_events), _i) for (_events, _i) in event_data_at_loc ]
+                event_data_at_loc = self.event_transforms(event_data_at_loc)
             
-            loc_image_data_at_split = [ self.image_paths[loc][s] for s in self.training_splits ]
-            images_at_t0 = [ (Image.open(loc_image_data_at_split[pos][i - 1]), i - 1) for pos, i in enumerate(index) ]
-            images_at_t1 = [ (Image.open(loc_image_data_at_split[pos][i]), i) for pos, i in enumerate(index) ]
+            loc_image_data_at_split = self.image_paths[loc][split]
+            # print('split and index', split, idx)
+            # print('image path for t0:', loc_image_data_at_split[idx - 1])
+            # exit()
+            image_at_t0 = Image.open(loc_image_data_at_split[idx - 1])
+            image_at_t1 = Image.open(loc_image_data_at_split[idx])
+            
+            # images_at_t0 = [ (Image.open(loc_image_data_at_split[pos][i - 1]), i - 1) for pos, i in enumerate(index) ]
+            # images_at_t1 = [ (Image.open(loc_image_data_at_split[pos][i]), i) for pos, i in enumerate(index) ]
 
             if self.image_transforms:
-                images_at_t0 = [ (self.image_transforms(img), i) for (img, i) in images_at_t0 ]
-                images_at_t1 = [ (self.image_transforms(img), i) for (img, i) in images_at_t1 ]
+                image_at_t0 = self.image_transforms(image_at_t0)
+                image_at_t1 = self.image_transforms(image_at_t1)
 
+            # print('min, max of image_at_t0:', image_at_t0.min(), image_at_t0.max())
+            # exit()
+            
             event_data[loc] = event_data_at_loc
-            image_data[loc] = { f'{s}': (images_at_t0[0], images_at_t1[0]) for s in self.training_splits }
+            image_data[loc] = (image_at_t0, image_at_t1)
             
         return (event_data, image_data)
+    
+    # def __getitem__(self, index):
+    #     image_data = {}
+    #     event_data = {}
+        
+    #     for loc in self.loc:
+            
+    #         loc_event_data: dict = self.event_data[loc]
+            
+    #         event_data_at_loc = [ (loc_event_data.get(s)[i], i) for s, i in zip(self.training_splits, index) ]
+            
+    #         if self.event_transforms:
+    #             event_data_at_loc = [ (self.event_transforms(_events), _i) for (_events, _i) in event_data_at_loc ]
+            
+    #         loc_image_data_at_split = [ self.image_paths[loc][s] for s in self.training_splits ]
+    #         images_at_t0 = [ (Image.open(loc_image_data_at_split[pos][i - 1]), i - 1) for pos, i in enumerate(index) ]
+    #         images_at_t1 = [ (Image.open(loc_image_data_at_split[pos][i]), i) for pos, i in enumerate(index) ]
+
+    #         if self.image_transforms:
+    #             images_at_t0 = [ (self.image_transforms(img), i) for (img, i) in images_at_t0 ]
+    #             images_at_t1 = [ (self.image_transforms(img), i) for (img, i) in images_at_t1 ]
+
+    #         event_data[loc] = event_data_at_loc
+    #         image_data[loc] = { f'{s}': (images_at_t0[0], images_at_t1[0]) for s in self.training_splits }
+            
+    #     return (event_data, image_data)
     
