b14aa6b095b0720723e2b38539cbd5d98c95eace
limsanky <sankarshana.v@gmail.com>
2025-10-27 08:46:14 +0000

    mvsec training script edited

--- mvsec_train.sh
+++ mvsec_train.sh
@@ -1,7 +1,8 @@
 
-DATE=1027
+DATE=1027_2
 BATCH_SIZE=12
 SAVE_FREQ=4
+SAVE_IMAGES=True
 
-python mvsec_ssl_event_representations.py --date $DATE --loss_type l1 --bs $BATCH_SIZE --save_freq $SAVE_FREQ
-python mvsec_ssl_event_representations.py --date $DATE --loss_type ph --bs $BATCH_SIZE --save_freq $SAVE_FREQ
+python mvsec_ssl_event_representations.py --date $DATE --loss_type l1 --bs $BATCH_SIZE --save_freq $SAVE_FREQ --save_images $SAVE_IMAGES
+python mvsec_ssl_event_representations.py --date $DATE --loss_type ph --bs $BATCH_SIZE --save_freq $SAVE_FREQ --save_images $SAVE_IMAGES
