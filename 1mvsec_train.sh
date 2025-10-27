bba7ff2b262b3713e071f497f8da90d5680813a3
limsanky <sankarshana.v@gmail.com>
2025-10-27 12:01:34 +0000

    more training scripts

---
+++ 1mvsec_train.sh
@@ -0,0 +1,9 @@
+
+DEVICES=1
+
+DATE=1027
+BATCH_SIZE=12
+SAVE_FREQ=4
+SAVE_IMAGES=True
+
+CUDA_VISIBLE_DEVICES=$DEVICES python mvsec_ssl_event_representations.py --date $DATE --loss_type ph --bs $BATCH_SIZE --save_freq $SAVE_FREQ --save_images $SAVE_IMAGES
--- mvsec_train.sh
+++ mvsec_train.sh
@@ -1,8 +1,9 @@
 
-DATE=1027_2
+DEVICES=0
+
+DATE=1027
 BATCH_SIZE=12
 SAVE_FREQ=4
 SAVE_IMAGES=True
 
-python mvsec_ssl_event_representations.py --date $DATE --loss_type l1 --bs $BATCH_SIZE --save_freq $SAVE_FREQ --save_images $SAVE_IMAGES
-python mvsec_ssl_event_representations.py --date $DATE --loss_type ph --bs $BATCH_SIZE --save_freq $SAVE_FREQ --save_images $SAVE_IMAGES
+CUDA_VISIBLE_DEVICES=$DEVICES python mvsec_ssl_event_representations.py --date $DATE --loss_type l1 --bs $BATCH_SIZE --save_freq $SAVE_FREQ --save_images $SAVE_IMAGES
